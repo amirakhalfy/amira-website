53af232ce899bfee56369dc28ef14b6e7f6b8ae8
Amira khalfi <amira.khalfi@esprit.tn>
2024-12-10 01:36:30 +0100

    Updates

---
+++ precache-manifest.ece167e160e156d36e64c1c229a1a0e3.js
@@ -0,0 +1,398 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89fb53cac510a5c3a61ec2a31591de8",
+    "url": "/amira-website/index.html"
+  },
+  {
+    "revision": "d9633342624e3b12202e",
+    "url": "/amira-website/static/css/main.d459e98f.chunk.css"
+  },
+  {
+    "revision": "dadb311149c78b4d5cd9",
+    "url": "/amira-website/static/js/2.42898f04.chunk.js"
+  },
+  {
+    "revision": "d9633342624e3b12202e",
+    "url": "/amira-website/static/js/main.7a88cea7.chunk.js"
+  },
+  {
+    "revision": "ac868e532b488654bb9d",
+    "url": "/amira-website/static/js/runtime-main.e21416dc.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/amira-website/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7680153d14c83395091b28ab92f578f1",
+    "url": "/amira-website/static/media/Frauddetectionsystem.7680153d.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/amira-website/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/amira-website/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/amira-website/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/amira-website/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/amira-website/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/amira-website/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "fdb8cde66590614e7282ef4ac5903693",
+    "url": "/amira-website/static/media/Jenkins.fdb8cde6.PNG"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/amira-website/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "5250f6f14ddb807a08a34cad5daf9cf0",
+    "url": "/amira-website/static/media/R.5250f6f1.png"
+  },
+  {
+    "revision": "5250f6f14ddb807a08a34cad5daf9cf0",
+    "url": "/amira-website/static/media/R_logo.5250f6f1.png"
+  },
+  {
+    "revision": "9bec5279bc93bfc309f42474a3db7534",
+    "url": "/amira-website/static/media/actuariallife.9bec5279.png"
+  },
+  {
+    "revision": "16559172ca1d4d552390b48f19e102ba",
+    "url": "/amira-website/static/media/address_image.16559172.svg"
+  },
+  {
+    "revision": "1a0b086c34b42f90e84569e263a2f579",
+    "url": "/amira-website/static/media/amira.1a0b086c.png"
+  },
+  {
+    "revision": "88b05abb8e969b50e903f0157516ad89",
+    "url": "/amira-website/static/media/amirakhalfi.88b05abb.png"
+  },
+  {
+    "revision": "ba47ecf297e0d0704a83ca40590f8384",
+    "url": "/amira-website/static/media/amour.ba47ecf2.png"
+  },
+  {
+    "revision": "10b53ab121145246bf51527a91783466",
+    "url": "/amira-website/static/media/amoura.10b53ab1.jpg"
+  },
+  {
+    "revision": "dcd6dee2bd7458a4a576dc131506fd83",
+    "url": "/amira-website/static/media/animated_logo.dcd6dee2.svg"
+  },
+  {
+    "revision": "e35df7b3f2bad9cf819a73da5fcf42f8",
+    "url": "/amira-website/static/media/animatedexp.e35df7b3.gif"
+  },
+  {
+    "revision": "2da81e92efec0b08142e0f1fe3d86064",
+    "url": "/amira-website/static/media/animatedscreen.2da81e92.gif"
+  },
+  {
+    "revision": "058eca4cbdc03a1dad9a6bb943ac7831",
+    "url": "/amira-website/static/media/blogs_image.058eca4c.svg"
+  },
+  {
+    "revision": "befecff8f89a63fde254b888c0f114b6",
+    "url": "/amira-website/static/media/bnalogo.befecff8.png"
+  },
+  {
+    "revision": "a24c0bad624136ad669a3eafa903c81b",
+    "url": "/amira-website/static/media/businesswoman.a24c0bad.png"
+  },
+  {
+    "revision": "01063dec48da5d5f863ec617717f375c",
+    "url": "/amira-website/static/media/ciscologo.01063dec.png"
+  },
+  {
+    "revision": "cacbc42b661f39344af81c631d8d36a9",
+    "url": "/amira-website/static/media/cloud_infrastructure.cacbc42b.svg"
+  },
+  {
+    "revision": "fee92c18b1cfbd1faa704c51047f113d",
+    "url": "/amira-website/static/media/codeInLogo.fee92c18.png"
+  },
+  {
+    "revision": "863144a738986ae236010bfd1df0b400",
+    "url": "/amira-website/static/media/computervisionproject.863144a7.png"
+  },
+  {
+    "revision": "e2e027455b9e30705a144a0057500310",
+    "url": "/amira-website/static/media/contact.e2e02745.png"
+  },
+  {
+    "revision": "5f915405511f649dad433a6db9adbff4",
+    "url": "/amira-website/static/media/contactMail.5f915405.png"
+  },
+  {
+    "revision": "ebf35d7f33ebbd4a4c6cff7e66e21fdb",
+    "url": "/amira-website/static/media/data_science.ebf35d7f.svg"
+  },
+  {
+    "revision": "75a7107b7739a46950f66de29779eb52",
+    "url": "/amira-website/static/media/deeplearning_ai_logo.75a7107b.png"
+  },
+  {
+    "revision": "3e23ca14e7a7c8a7b63d255466749000",
+    "url": "/amira-website/static/media/developerActivity.3e23ca14.svg"
+  },
+  {
+    "revision": "82b72e4fc28a42be4e2d0d06bce30934",
+    "url": "/amira-website/static/media/devops.82b72e4f.png"
+  },
+  {
+    "revision": "6208702c7f0af3be1422d3979e4192d1",
+    "url": "/amira-website/static/media/dockerhub.6208702c.PNG"
+  },
+  {
+    "revision": "63f707f224c8a842ac928c36e3be879a",
+    "url": "/amira-website/static/media/dsc_logo.63f707f2.png"
+  },
+  {
+    "revision": "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    "url": "/amira-website/static/media/education.3bab7a9d.svg"
+  },
+  {
+    "revision": "b8fedba09b2ed3b02119cec6ee302c3f",
+    "url": "/amira-website/static/media/espritlogo.b8fedba0.png"
+  },
+  {
+    "revision": "a00ede3f70759480e81f14f203edaa51",
+    "url": "/amira-website/static/media/experience.a00ede3f.svg"
+  },
+  {
+    "revision": "0017cc6de3f72f67753efa37bb796fbd",
+    "url": "/amira-website/static/media/fa-brands-400.0017cc6d.svg"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/amira-website/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/amira-website/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/amira-website/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/amira-website/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/amira-website/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/amira-website/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/amira-website/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "e647ddbb6d95cb01deac07ffafb2a085",
+    "url": "/amira-website/static/media/fa-regular-400.e647ddbb.svg"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/amira-website/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/amira-website/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/amira-website/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "424c59fb1df79fcc3d0a41909c0192d2",
+    "url": "/amira-website/static/media/fa-solid-900.424c59fb.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/amira-website/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/amira-website/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "2936ce95f550e3feef7230ea543a4b60",
+    "url": "/amira-website/static/media/feelingProud.2936ce95.svg"
+  },
+  {
+    "revision": "80197905ed40fec733cd29442a2abefe",
+    "url": "/amira-website/static/media/finetune.80197905.png"
+  },
+  {
+    "revision": "d9efc1fda752b7c651045cc91f8bfdfb",
+    "url": "/amira-website/static/media/flutter.d9efc1fd.png"
+  },
+  {
+    "revision": "82c4753917fda7ae3d37e0c6cb1665c3",
+    "url": "/amira-website/static/media/full_stack_development_about.82c47539.svg"
+  },
+  {
+    "revision": "fce6080726685cabdb4f75a0d8018325",
+    "url": "/amira-website/static/media/fullstack.fce60807.svg"
+  },
+  {
+    "revision": "0ee7e8c9444cdfefc10f6d86e18b9b5b",
+    "url": "/amira-website/static/media/gcp_logo.0ee7e8c9.png"
+  },
+  {
+    "revision": "6eeb2e810d0fd9f3ca2dcd72de228e68",
+    "url": "/amira-website/static/media/github_logo.6eeb2e81.png"
+  },
+  {
+    "revision": "0c8a4c114ad00f7854e91550379b9b91",
+    "url": "/amira-website/static/media/googleAssistant.0c8a4c11.jpg"
+  },
+  {
+    "revision": "a6addc3fb6605480614b992ede8cd434",
+    "url": "/amira-website/static/media/googleAssistant.a6addc3f.svg"
+  },
+  {
+    "revision": "93e1c34d710bc4e0d3c5f2de43b84ac2",
+    "url": "/amira-website/static/media/handdrawn.93e1c34d.png"
+  },
+  {
+    "revision": "d3fccbe1db76b96f4f320d9c0f7da30c",
+    "url": "/amira-website/static/media/ibm_logo.d3fccbe1.png"
+  },
+  {
+    "revision": "571a8584e69ecfbe326c8b8385fbbf54",
+    "url": "/amira-website/static/media/iiitk_logo.571a8584.png"
+  },
+  {
+    "revision": "29152cf756a1facead5393c32e373371",
+    "url": "/amira-website/static/media/intel_logo.29152cf7.jpg"
+  },
+  {
+    "revision": "60ed0bd3f8b99ac1d29e74575f804aaa",
+    "url": "/amira-website/static/media/iu_logo.60ed0bd3.png"
+  },
+  {
+    "revision": "5b4a6449747ec61a6d9aa874f2a9ceba",
+    "url": "/amira-website/static/media/jsFramework.5b4a6449.svg"
+  },
+  {
+    "revision": "c597fbded5de799dfb9abc3fc4f1c26f",
+    "url": "/amira-website/static/media/legato_logo.c597fbde.png"
+  },
+  {
+    "revision": "5ab3b50f10becd3136f12ddf8e9db2a8",
+    "url": "/amira-website/static/media/lemans.5ab3b50f.png"
+  },
+  {
+    "revision": "487552f26324c3ce45a13f48f4f4ebb6",
+    "url": "/amira-website/static/media/loan.487552f2.png"
+  },
+  {
+    "revision": "800d6f65ba149bee75a2e64b89fe368c",
+    "url": "/amira-website/static/media/logobfi.800d6f65.png"
+  },
+  {
+    "revision": "83ac24fb968a474c6a63f7a4e9a8f62c",
+    "url": "/amira-website/static/media/manOnTable.83ac24fb.svg"
+  },
+  {
+    "revision": "187d3cfcfcade3bc068e6b0a9df8d2c5",
+    "url": "/amira-website/static/media/microsoftlogo.187d3cfc.png"
+  },
+  {
+    "revision": "a4f0f2b10165f6c6453ff16e210001f1",
+    "url": "/amira-website/static/media/mira.a4f0f2b1.png"
+  },
+  {
+    "revision": "d06e092b810dfaed4be227abcff08050",
+    "url": "/amira-website/static/media/montecarlo.d06e092b.png"
+  },
+  {
+    "revision": "88a0a94d99af1f78ad2aa3f3b2a49497",
+    "url": "/amira-website/static/media/mslogo.88a0a94d.png"
+  },
+  {
+    "revision": "069938a84edd6ebf383c318e9feb5d4c",
+    "url": "/amira-website/static/media/muffito_logo.069938a8.png"
+  },
+  {
+    "revision": "95a12a2b8c3149cce2175679755c4b4d",
+    "url": "/amira-website/static/media/nptel_logo.95a12a2b.png"
+  },
+  {
+    "revision": "f48bd143bdfa6b1404d80d39e7326f05",
+    "url": "/amira-website/static/media/nvidialogo.f48bd143.png"
+  },
+  {
+    "revision": "414dfbd98516b39255a3e22ed90b3f7d",
+    "url": "/amira-website/static/media/portfolio.414dfbd9.gif"
+  },
+  {
+    "revision": "38c2c67c94986da39c98d5b8a2d52c4e",
+    "url": "/amira-website/static/media/profile.38c2c67c.png"
+  },
+  {
+    "revision": "5faf790badc18fa9030f94836ece1df1",
+    "url": "/amira-website/static/media/programmer.5faf790b.svg"
+  },
+  {
+    "revision": "b8ba948796d7ab532673c5ed2f315e74",
+    "url": "/amira-website/static/media/projects_image.b8ba9487.svg"
+  },
+  {
+    "revision": "71408db71465afd742233e4a96f06526",
+    "url": "/amira-website/static/media/pwa.71408db7.png"
+  },
+  {
+    "revision": "fa5af6c28a09c7a6e99aaad18b7d7ea8",
+    "url": "/amira-website/static/media/recuperernexus.fa5af6c2.PNG"
+  },
+  {
+    "revision": "2c497738be3831492aeb6bdc43f19154",
+    "url": "/amira-website/static/media/stanford_logo.2c497738.png"
+  },
+  {
+    "revision": "0e6f21ca6646107b6b2e504827f0ce36",
+    "url": "/amira-website/static/media/statistic.0e6f21ca.png"
+  },
+  {
+    "revision": "6ac91b161c2c9934a44f72d75d18142d",
+    "url": "/amira-website/static/media/talksCardBack.6ac91b16.svg"
+  },
+  {
+    "revision": "3fa5424232370e3d049c9555d9c440a4",
+    "url": "/amira-website/static/media/talksCardBackAlt.3fa54242.svg"
+  },
+  {
+    "revision": "0993e3246bf5146154391a0afef79b03",
+    "url": "/amira-website/static/media/tiktok_logo.0993e324.png"
+  },
+  {
+    "revision": "d63630893eb8cb64af65f395d393c481",
+    "url": "/amira-website/static/media/ui_ux_design.d6363089.svg"
+  },
+  {
+    "revision": "fdc544a6d743aff3893866cf36e54bc4",
+    "url": "/amira-website/static/media/velo.fdc544a6.PNG"
+  }
+]);
